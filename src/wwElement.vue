216847e244a71f9174f5de89fffa413b22b576f2
timkriz <tim.kriznik@gmail.com>
2023-08-06 18:17:29 +0200

    Fisrt commit

---
+++ src/wwElement.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="my-element">
+    <p>Hello"</p>
+    <v-chip-group
+        v-model="internalStateModel"
+        column
+        multiple
+      >
+        <v-chip
+          v-for="(option, i) in initialOptions"
+          :key="i"
+          :value="option.value"
+          selected-class="selected-chip"
+          size="large"
+          filter
+          filter-icon="$account"
+          variant="outlined"
+        >
+          {{option.label.en}}
+        </v-chip>
+      </v-chip-group>
+  </div>
+</template>
+
+<script>
+import { ref, computed, getCurrentInstance, watch, toRefs } from 'vue';
+import { VChip } from 'vuetify/components/VChip';
+import { VChipGroup } from 'vuetify/components/VChipGroup';
+import { createVuetify } from 'vuetify'
+import { Ripple } from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+import { mdiCheckCircleOutline } from '@mdi/js';
+import 'vuetify/styles';
+
+export default {
+  props: {
+    content: { type: Object, required: true },
+  },
+  emits: ['trigger-event'],
+  setup(props, { emit }) {
+    const { options: optionsRef } = toRefs(props.content);
+    const app = getCurrentInstance()
+
+    const vuetify = createVuetify({
+      components: {
+        VChipGroup,
+        VChip,
+      },
+      directives: {
+        Ripple,
+      },
+      icons: {
+        defaultSet: 'mdi',
+        aliases: {
+          ...aliases,
+          account: mdiCheckCircleOutline,
+        },
+        sets: {
+          mdi,
+        },
+      },
+    })
+    app.appContext.app.use(vuetify);
+    
+    const initialOptions = ref(Array.isArray(props.content.options) ? [...props.content.options] : []);
+    watch(optionsRef, async (newOptions, oldOptions) => {
+        initialOptions.value = Array.isArray(newOptions) ? [...newOptions] : [];
+    })
+
+    const emitChangeEvent = (newValue) => {
+      const eventPayload = { name: 'change', event: { domEvent: {}, value: newValue } };
+      emit('trigger-event', eventPayload);
+    };
+
+    const internalState = ref([]);
+
+    // Create a computed property with a getter and setter
+    const internalStateModel = computed({
+      get() {
+        // Additional processing when getting the value
+        return internalState.value;
+      },
+      set(newValue) {
+        // Additional processing before setting the value
+        internalState.value = newValue;
+        emitChangeEvent(newValue);
+      },
+    });
+
+    return {
+      initialOptions,
+      internalStateModel,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.selected-chip {
+  border: 0;
+  color: white;
+  background: linear-gradient(90deg,#ef407c, #f2736c);
+}
+.v-chip:not(.selected-chip) {
+  color: black;
+  background: white;
+}
+</style>
---
+++ ww-config.js
@@ -0,0 +1,50 @@
+export default {
+  editor: {
+    label: {
+      en: "My Element",
+    },
+  },
+  triggerEvents: [
+    { name: 'change', label: { en: 'On change' }, event: { value: [] }, default: true },
+],
+properties: {
+    options: {
+        label: { en: 'Options', fr: 'Options' },
+        type: 'Array',
+        section: 'settings',
+        options: {
+            item: {
+                type: 'Object',
+                defaultValue: { label: 'New option', value: '' },
+                options: {
+                    item: {
+                        label: {
+                            label: { en: 'Label' },
+                            type: 'Text',
+                            options: { placeholder: 'Label' },
+                            multiLang: true,
+                        },
+                        value: {
+                            label: { en: 'Value' },
+                            type: 'Text',
+                            options: { placeholder: 'Value' },
+                        },
+                    },
+                },
+            },
+        },
+        defaultValue: [
+            { value: 'option', label: { en: 'option - 1' } },
+            { value: 'option2', label: { en: 'option - 2' } },
+        ],
+        bindable: true,
+        /* wwEditor:start */
+        bindingValidation: {
+            type: 'array',
+            tooltip:
+                'A collection of data in array format: \n\n `[{}, {}, ...] || ["string1", "string2", ...] || [1, 2, ...]`',
+        },
+        /* wwEditor:end */
+    },
+},
+};
